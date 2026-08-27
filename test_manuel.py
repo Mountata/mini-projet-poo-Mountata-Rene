011656a8f80dd9981becdd4f3ebd25feb6cf3f97
Mountata <renelegrandmountata@gmail.com>
2026-08-27 00:22:23 +0000

    test manuel

---
+++ test_manuel.py
@@ -0,0 +1,9 @@
+from mediatheque.documents import Livre, DVD
+
+l = Livre("L'Aventure ambiguë", 1961, "L001", auteur="Cheikh Hamidou Kane", nb_pages=191)
+print(l)
+print(l.duree_pret())
+
+d = DVD("Camp de Thiaroye", 1988, "D001", realisateur="Sembene Ousmane", duree_min=147)
+print(d)
+print(d.duree_pret())
